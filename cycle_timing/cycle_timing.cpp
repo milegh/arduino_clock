8d2c140e3d236a4602d8874529a8f926a50ad2f9
mugi <mugi@gscc.net.tw>
2016-12-15 14:06:41 +0800

    1.0.2
    save

--- cycle_timing/cycle_timing.cpp
+++ cycle_timing/cycle_timing.cpp
@@ -13,7 +13,6 @@ long cycle_timing::gat_clock() {
   }
   
 
-	
 void cycle_timing::setup_clock(unsigned long cycle,unsigned long nowTime){
 	m_cycle = cycle;
 	offset = nowTime - (millis() / 1000)% cycle;
